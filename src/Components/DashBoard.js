e48652b866d0e18550847962015266bffa8b2ad1
kenan aljaber <kenan.aljaber.p@gmail.com>
2022-11-22 19:29:14 -0500

    El proyecto de calcular los años meses y dias desde el nacimiento de uno

---
+++ src/Components/DashBoard.js
@@ -0,0 +1,11 @@
+import DatePicker from "./DatePicker";
+
+
+const DashBoard = () => {
+    return (<div className="dashboard">
+        <DatePicker lableText="select date of birth:"/>
+
+    </div>  );
+}
+ 
+export default DashBoard;
---
+++ src/Components/Header.js
@@ -0,0 +1,7 @@
+const Header = ({headerText}) => {
+    return ( <div className="headerContainer">
+        <h1>{headerText}</h1>
+    </div> );
+}
+ 
+export default Header;
